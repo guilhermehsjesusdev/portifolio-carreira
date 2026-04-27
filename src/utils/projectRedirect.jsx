e7fff6fd253bec6ec34610244b798cca10c8b073
Guilherme Henrique de Sousa Jesus <guilherme26h@gmail.com>
2026-04-27 20:36:33 -0300

    Atualização projeto segurança

--- src/utils/projectRedirect.jsx
+++ src/utils/projectRedirect.jsx
@@ -3,3 +3,7 @@
 export const redirectBlog = () => {
   window.open("https://devsec-miniblog-oo5tpwxwi.vercel.app/", "_blank");
 }
+
+export const redirectSecurity = () => {
+  window.open("https://github.com/guilhermehsjesusdev/laboratorio-seguranca/", "_blank");
+}
